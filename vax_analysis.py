3867174d3ef7e7be9c356487d32a4b349b2c91b6
aah-41350 <149913164+aah-41350@users.noreply.github.com>
2023-11-30 22:28:10 +0800

    Cleaning of dataset almost done
    Can do basic analytics, but for clearer picture need to work on dates of the data

---
+++ vax_analysis.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 08:13:05 2023
+
+@author: azfar
+"""
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn
+
+filepath = '/Users/azfar/Documents/GitHub/group-project/Data Files'
+
+# Import original datasets into specified DataFrames
+df_t_vax = pd.read_csv(filepath+'/vax_malaysia.csv')
+df_aefi = pd.read_csv(filepath+'/aefi.csv')
+
+# Truncate unnecessary columns
+df_t_vax.drop(columns=['cansino','cansino3','cansino4','pending1','pending2',
+                       'pending3','pending4'], inplace=True)
+df_t_vax.drop(columns=['daily_partial','daily_full','daily_booster',
+                       'daily_booster2'], inplace=True)
+df_t_vax.drop(df_t_vax.columns[[2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,
+                                20,21,22]], axis=1, inplace=True)
+
+# Consolidate the different vaccine doses into single columns
+cols = ['pfizer1','pfizer2','pfizer3','pfizer4']
+df_t_vax['pfizer'] = df_t_vax[cols].sum(1)
+df_t_vax.drop(cols, axis=1, inplace=True)
+
+cols1 = ['sinovac1','sinovac2','sinovac3','sinovac4']
+df_t_vax['sinovac'] = df_t_vax[cols1].sum(1)
+df_t_vax.drop(cols1, axis=1, inplace=True)
+
+cols2 = ['astra1','astra2','astra3','astra4']
+df_t_vax['astra'] = df_t_vax[cols2].sum(1)
+df_t_vax.drop(cols2, axis=1, inplace=True)
+
+cols3 = ['sinopharm1','sinopharm2','sinopharm3','sinopharm4']
+df_t_vax['sinopharm'] = df_t_vax[cols3].sum(1)
+df_t_vax.drop(cols3, axis=1, inplace=True)
+
+# Save the edited DataFrame into a new .csv file
+df_t_vax.to_csv(filepath+'/totalvax.csv', index=False)
+
+total_pfizer = df_t_vax['pfizer'].sum()
+total_astra = df_t_vax['astra'].sum()
+total_sinovac = df_t_vax['sinovac'].sum()
+total_sinopharm = df_t_vax['sinopharm'].sum()
+
+df_doses = pd.DataFrame({
+    'pfizer' : total_pfizer,
+    'astrazeneca' : total_astra,
+    'sinovac' : total_sinovac,
+    'sinopharm' : total_sinopharm},
+    index=['total_doses'])
+
+# Cleaning AEFI dataset
+vaxgroups = df_aefi.groupby('vaxtype')
+aefi_p = vaxgroups.get_group('pfizer').drop(columns=['date','vaxtype']).sum()
+aefi_a = vaxgroups.get_group('astrazeneca').drop(columns=['date','vaxtype']).sum()
+aefi_sv = vaxgroups.get_group('sinovac').drop(columns=['date','vaxtype']).sum()
+aefi_sp = vaxgroups.get_group('sinopharm').drop(columns=['date','vaxtype']).sum()
+
+# New DataFrame for cummulative sum of reported AEFI, sorted by brand
+df_aefi_total = pd.DataFrame({
+    'pfizer' : aefi_p,
+    'astrazeneca' : aefi_a,
+    'sinovac' : aefi_sv,
+    'sinopharm' : aefi_sp
+    })
+
+df_clean = pd.concat([df_aefi_total, df_doses])
+df_clean.to_csv(filepath+'/for_analysis.csv', index=False)
